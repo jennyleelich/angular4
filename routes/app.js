ccbfdf12aec1cbc9f833848acea0d38bfa05c4d7
jenny lee <jenny.lee4678@gmail.com>
2017-12-15 12:22:31 -0500

    prepare angular project, deleted node.hbs file and modified routes/app.js

--- routes/app.js
+++ routes/app.js
@@ -1,26 +1,14 @@
 var express = require('express');
 var router = express.Router();
-var User = require('../models/user');
-var Message = require('../models/message');
 
 router.get('/', function (req, res, next) {
-    User.findOne({},function(err,doc){
-        if(err){
-            return res.send('Error!');
-        }
-        res.render('node',{email:doc.email});
-    })
-  //  res.render('node');
+    // User.findOne({},function(err,doc){
+    //     if(err){
+    //         return res.send('Error!');
+    //     }
+    //     res.render('node',{email:doc.email});
+    // })
+   res.render('index');
    });
-router.post('/',function(req,res,next){
-    var email = req.body.email;
-    var user = new User({
-        firstName :'Max',
-        lastName: 'li',
-        passWord:'123',
-        email:email
-    });
-    user.save();
-    res.redirect('/');
-})
+
 module.exports = router;
