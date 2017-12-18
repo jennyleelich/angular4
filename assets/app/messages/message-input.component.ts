ac60f14172f4601965baff9d7cf1a5577b2345d5
jenny lee <jenny.lee4678@gmail.com>
2017-12-18 15:56:18 -0500

    adding message with a template driven form

--- assets/app/messages/message-input.component.ts
+++ assets/app/messages/message-input.component.ts
@@ -1,24 +1,21 @@
 import { Component } from "@angular/core";
 import { Message } from "./message.model";
 import { MessageService } from "./message.service";
+import { NgForm } from "@angular/forms";
 @Component({
     selector: 'app-message-input',
-    template:`
-        <div class="col-md-8 col-md-offset-2" >
-            <div class="form-group">
-                <label for="content">Content</label>
-                <input type="text" id="content" class="form-control" #input>
-            </div>
-            <button class="btn btn-primary" type="submit" (click)="onSave(input.value)">Save</button>
-        </div>
+    templateUrl: "./message-input.component.html"
+        
     
-    `
+    
 })
 export class MessageInputComponent {
     constructor(private messageService: MessageService){}
     inputValue:any;
-    onSave(value:any){
-        const message = new Message(value, 'Max');
+    onSubmit(form: NgForm){
+        //console.log(form);
+        const message = new Message(form.value.content, 'Max');
         this.messageService.addMessage( message);
+        form.resetForm();
     }
 }
