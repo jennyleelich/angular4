354e7c15752c4381a436f90899f3877c30dc1009
jenny lee <jenny.lee4678@gmail.com>
2017-12-19 15:34:08 -0500

    adding routing two directive very important routerLinkActive and routerLink

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -6,14 +6,21 @@ import { MessageComponent } from "./messages/message.component";
 import { MessageListComponent } from "./messages/message-list.component";
 import { MessageInputComponent } from "./messages/message-input.component";
 import { MessageService } from "./messages/message.service";
+import { MessagesComponent } from "./messages/messages.component";
+import { AuthenticationComponent } from "./auth/authentication.component";
+import { HeaderComponent } from "./header/header.component";
+import { routing } from "./app.routing";
 @NgModule({
     declarations: [
         AppComponent,
         MessageComponent,
         MessageListComponent,
-        MessageInputComponent
+        MessageInputComponent,
+        MessagesComponent,
+        AuthenticationComponent,
+        HeaderComponent
     ],
-    imports: [BrowserModule,FormsModule],
+    imports: [BrowserModule,FormsModule, routing],
     bootstrap: [AppComponent],
     providers: [ MessageService ]
 })
