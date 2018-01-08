4b9247681448fb739d26c9caa6130c6269c0381e
jenny lee <jenny.lee4678@gmail.com>
2018-01-08 11:35:17 -0500

    sign in backend

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var User = require('../models/user');
 var bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
 
 router.post('/', function (req, res, next) {
     var user = new User({
@@ -23,5 +24,33 @@ router.post('/', function (req, res, next) {
         });
     });
    });
-
+router.post('/signin',function(req,res,next){
+    User.findOne({email:req.body.email},function(err,user){
+        if(err){
+            return res.status(500).json({
+                title:'An error occured',
+                error:err
+            });
+        }
+        if(!user){
+            return res.status(401).json({
+                title:'Login failed',
+                error:{message: 'Invalid login credentials'}
+            });
+        }
+        if(!bcrpt.compareSync(req.body.passWord,user.passWord)){
+            return res.status(401).json({
+                title:'Login failed',
+                error:{message: 'Invalid login credentials'}
+            });
+        }
+        var token = jwt.sign({user:user},'secret',{expireIn:7200});//created a new token and signs it for us, generate and sign it
+        res.status(200).json({
+            message:'Successfully logged in',
+            token: token,
+            userId: user._id
+        })
+        
+    })
+})
 module.exports = router;
