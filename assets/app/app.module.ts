f939f82e5d048a8c723b3d8f10bee34ad6919423
jenny lee <jenny.lee4678@gmail.com>
2018-01-11 12:14:13 -0500

    adding auth module auth routing to lazy load the auth module, and adding message module to make the app more lean

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -1,39 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule , ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from "@angular/http";
 import { AppComponent } from "./app.component";
-import { MessageComponent } from "./messages/message.component";
-import { MessageListComponent } from "./messages/message-list.component";
-import { MessageInputComponent } from "./messages/message-input.component";
-import { MessageService } from "./messages/message.service";
 import { AuthService } from "./auth/auth.service";
 import { ErrorService } from "./errors/error.service";
-import { MessagesComponent } from "./messages/messages.component";
 import { AuthenticationComponent } from "./auth/authentication.component";
 import { HeaderComponent } from "./header/header.component";
-import { SigninComponent } from "./auth/signin.component";
-import { SignupComponent } from "./auth/signup.component";
-import { LogoutComponent } from "./auth/logout.component";
 import { ErrorComponent } from "./errors/error.component";
+import { MessageModule } from "./messages/message.module";
+
 import { routing } from "./app.routing";
 
 @NgModule({
     declarations: [
         AppComponent,
-        MessageComponent,
-        MessageListComponent,
-        MessageInputComponent,
-        MessagesComponent,
         AuthenticationComponent,
         HeaderComponent,
-        LogoutComponent,
-        SigninComponent,
-        SignupComponent,
         ErrorComponent
     ],
-    imports: [BrowserModule,FormsModule, routing, ReactiveFormsModule, HttpModule],
-    providers: [ MessageService,AuthService,ErrorService ],
+    imports: [
+         BrowserModule,
+         routing, 
+         HttpModule,
+         MessageModule ],
+    providers: [ AuthService,ErrorService ],
     bootstrap: [AppComponent]
 })
 export class AppModule {
